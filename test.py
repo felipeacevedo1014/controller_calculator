e4fcaca060fd985654419a1a2866f4d11dac4903
felipeacevedo1014 <felipeacevedo1014@gmail.com>
2025-07-03 10:22:43 -0500

    change gui size

--- test.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-dc={"Name":["Luis","Felipe"],"Last_name":["Acevedo","Gonzalez"]}
-dataframe=pd.DataFrame(dc)
-list=dataframe.values
-print(list)
-print(type(list))
--- test2.py
+++
@@ -1,6 +0,0 @@
-import tkinter as tk
-
-root = tk.Tk()
-root.geometry("300x100")
-tk.Label(root, text="Does this show?").pack(pady=20)
-root.mainloop()
